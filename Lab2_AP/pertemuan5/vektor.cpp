de55f2eddd9a55a69a5dff06f918773fb33fad19
jefrialbukhori <bangjeff200505@gmail.com>
2025-04-25 15:42:41 +0700

    pertemuan 5 : array,vektor

---
+++ Lab2_AP/pertemuan5/vektor.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+    // Vector Declaration & Initialization
+    vector<string> nama_karyawan = {"nolan", "julian", "lukas", "lancelot"};
+
+    // Ascending Element of Vector
+    // for (int i=0; i < nama_karyawan.size(); i++){
+    //     cout << nama_karyawan[i] << endl;
+    // }
+
+    // for (string karyawan : nama_karyawan){
+    //     cout << karyawan << endl;
+    // }
+
+    // Add data to vector
+    // nama_karyawan.push_back("imam");
+    // for (int i=0; i < nama_karyawan.size(); i++){
+    //     cout << nama_karyawan[i] << endl;
+    // }
+
+    // Delete data from vector
+    // nama_karyawan.pop_back();
+    // nama_karyawan.erase(nama_karyawan.begin() + 3);
+    
+    // for (int i=0; i < nama_karyawan.size(); i++){
+    //     cout << nama_karyawan[i] << endl;
+    //}
+
+}
